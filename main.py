16a6803bafd7fde344cdde1f7bc8990c58886eee
Garrise <dan19920316@gmail.com>
2025-01-06 05:14:00 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -52,3 +52,40 @@ class BiliAnalysisPlugin(BasePlugin):
                 await ctx.send_message(ctx.event.launcher_type, str(ctx.event.launcher_id),["视频解析失败"])
                 ctx.prevent_default()
                 ctx.prevent_postorder()
+        else:
+            #处理非永久短链
+            url_match = re.search(r'(https://b23.tv/\w+)', msg) or re.search(r'(https://bili2233.cn/\w+)', msg)
+            if url_match:
+                url_parse = url_match.group(1)
+            response = requests.head(url_parse, allow_redirects=True)
+            if 'location' in response.headers:
+                url = response.headers['location']
+            else:
+                url = response.url
+            bv_match = re.search(r'www.bilibili.com/video/(BV\w+)', url)
+            if bv_match:
+                id = bv_match.group(1)
+                req = f"bvid={id}"
+                headers = {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'
+                }
+                response = requests.get(f"https://api.bilibili.com/x/web-interface/view?{req}", headers=headers)
+                data = response.json()
+                if data['code'] == 0:
+                    video_data = data['data']
+                    cover_url = video_data['pic']
+                    author_name = video_data['owner']['name']
+                    video_url = "https://www.bilibili.com/video/" + id
+                    title = video_data['title']
+                    await ctx.send_message(ctx.event.launcher_type, str(ctx.event.launcher_id),MessageChain([
+                                                                                                            Image(url=cover_url),
+                                                                                                            f"视频标题：{title}\n",
+                                                                                                            f"UP主：{author_name}\n",
+                                                                                                            f"视频链接：{video_url}"
+                                                                                                            ]))
+                    ctx.prevent_default()
+                    ctx.prevent_postorder()
+                else:
+                    await ctx.send_message(ctx.event.launcher_type, str(ctx.event.launcher_id),["视频解析失败"])
+                    ctx.prevent_default()
+                    ctx.prevent_postorder()
